69f203a0e5b75833279eaeaadc564efd81bcee65
kuru9696 <kurumi.inao@gmail.com>
2025-12-16 14:09:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-ver = L.map('map').setView([35.70059963531073, 139.49998981936298], 16); 
+var = L.map('map').setView([35.70059963531073, 139.49998981936298], 16); 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
