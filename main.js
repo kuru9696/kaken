bf659febc28fd41ed0baf48067c98bec8e04fd32
kuru9696 <kurumi.inao@gmail.com>
2026-01-13 14:46:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-var = L.map('map').setView([35.70059963531073, 139.49998981936298], 16); 
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css">
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" defer></script>
